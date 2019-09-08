be0aea7cc6cd57d0814d1fa644a12559bf8b09a4
jingechen <jingechen@sohu-inc.com>
2019-09-08 11:27:07 +0800

    add:轮询setInterval

---
+++ 1.query/app.js
@@ -0,0 +1,9 @@
+let express = require("express");
+let app = express();
+
+// http://localhost:8080
+app.use(express.static(__dirname));
+app.get("/clock",function(req,res){
+    res.send(new Date().toLocaleString());
+});
+app.listen(8080);
