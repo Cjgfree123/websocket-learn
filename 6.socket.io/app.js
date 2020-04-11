5e9518376182b73e7104d4162aefa6c18d07a955
jingechen <cjg_free@163.com>
2020-04-11 18:32:51 +0800

    add:基本聊天室展示

--- 6.socket.io/app.js
+++ 6.socket.io/app.js
@@ -7,8 +7,9 @@ app.use(express.static(__dirname));
 let server = require("http").createServer(app);
 // server(属于必须)用于返回数据, socket用于实时通信
 let io = require("socket.io")(server);
+
 // 监听客户端的连接事件，当客户端连接上来后，执行回调函数
-// 默认路径 io("/")
+// 默认路径 io("/") 即: io.on("connection", func()) 是io.of("/").on("connection", func())的简写
 io.on("connection", function(socket){
     console.log("服务器接收到客户端的连接");
     socket.on("message", function(message){
